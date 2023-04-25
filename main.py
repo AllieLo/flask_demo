527cfd301736ae1c17935ee9ddcd64c90ef505d9
allie <adaipoo@gmail.com>
2023-04-25 20:21:28 +0800

    完成排序功能(使用GET & POST)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 from pm25 import get_pm25
 
@@ -6,44 +6,51 @@ from pm25 import get_pm25
 app = Flask(__name__)
 
 stocks = [
-    {'分類': '日經指數', '指數': '22,920.30'},
-    {'分類': '韓國綜合', '指數': '2,304.59'},
-    {'分類': '香港恆生', '指數': '25,083.71'},
-    {'分類': '上海綜合', '指數': '3,380.68'}
+    {"分類": "日經指數", "指數": "22,920.30"},
+    {"分類": "韓國綜合", "指數": "2,304.59"},
+    {"分類": "香港恆生", "指數": "25,083.71"},
+    {"分類": "上海綜合", "指數": "3,380.68"},
 ]
 
 # 建立首頁：函式綁定網頁
 
 
-@app.route('/pm25')
+@app.route("/pm25", methods=["GET", "POST"])
 def pm25():
-    columns, values = get_pm25()
-    return render_template('./pm25.html', **locals())
+    if request.method == "GET":
+        columns, values = get_pm25()
+        # 單純使用GET才能這樣寫
+        # if request.args.get('sort'):
+        #     columns, values = get_pm25(True)
+    if request.method == "POST":
+        if request.form.get("sort"):
+            columns, values = get_pm25(True)
+        else:
+            columns, values = get_pm25()
+    return render_template("./pm25.html", **locals())
 
 
-@app.route('/stock')
+@app.route("/stock")
 def stock():
-
-    return render_template('./stock.html', stocks=stocks,
-                           datetime=get_date())
+    return render_template("./stock.html", stocks=stocks, datetime=get_date())
 
 
-@app.route('/index')
-@app.route('/')             # @route-->首頁
+@app.route("/index")
+@app.route("/")  # @route-->首頁
 def index():
-    name = 'guest'
+    name = "guest"
     date = get_date()
-    content = {'name': name, 'date': date}
-    return render_template('./index.html', content=content)
+    content = {"name": name, "date": date}
+    return render_template("./index.html", content=content)
 
 
-@app.route('/date')
+@app.route("/date")
 def get_date():
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-@app.route('/book')
-@app.route('/book/<int:id>')
+@app.route("/book")
+@app.route("/book/<int:id>")
 def get_book(id=None):
     try:
         books = {1: "HTML-Book", 2: "CSS-Book", 3: "JavaScript-Book"}
@@ -53,8 +60,8 @@ def get_book(id=None):
             return books
     except Exception as e:
         print(e)
-        return '<h2>查詢錯誤，請重新輸入</h2>'
+        return "<h2>查詢錯誤，請重新輸入</h2>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- pm25.py
+++ pm25.py
@@ -1,16 +1,26 @@
 import requests
 
 
-def get_pm25():
+def get_pm25(sort=False):
     columns, values = None, None
     try:
         url = 'https://data.epa.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
         resp = requests.get(url)
         datas = resp.json()['records']
+        # 組合標題
         columns = list(datas[0].keys())
+        # 組合內容
         values = []
         for data in datas:
-            values.append(list(data.values()))
+            data = list(data.values())
+            try:
+                data[2] = eval(data[2])
+                values.append(data)
+            except Exception as e:
+                print(e)
+        if sort:
+            values = sorted(values, key=lambda x: x[2], reverse=True)
+        print(values)
 
     except Exception as e:
         print(e)
